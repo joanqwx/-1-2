a44cdceb2fa0ed4b08a9483f0f24e961a7e7cb47
joanqwx <dannagoddess.12worlds@gmail.com>
2025-07-17 18:27:22 +0500

    Add files via upload

---
+++ 2zad.py
@@ -0,0 +1,8 @@
+a="Australopithecus afarensis"
+b="Homo habilis"
+c="Homo erectus"
+d="Homo neanderthalensis"
+e="Homo sapiens"
+g="Homo sapiens sapiens"
+print("Эволюционные этапы развития человека:",a, b, c, d, e, g, sep="=>")
+
